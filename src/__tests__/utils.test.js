0393f8a565ecdf9b8732081a874a9a91df5e18a6
Nikki Little <nikkilittle13@gmail.com>
2024-03-19 20:33:48 -0400

    completed lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,10 +2,18 @@ import { pointsForWord } from "../utils";
 
 describe("pointsForWord", () => {
   it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
-    const word = "test";
+    const word = "tEsT";
 
     const points = pointsForWord(word);
 
     expect(points).toBe(7);
   });
+});
+
+it("handles uppercase and lowercase input", () => {
+  const word = "tEsT";
+
+  const points = pointsForWord(word);
+
+  expect(points).toBe(7);
 });
